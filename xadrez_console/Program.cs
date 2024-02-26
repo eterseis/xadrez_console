2182d5049a3639d8f810f62be1e13d438bed3a68
Éter <github.italicize027@passinbox.com>
2024-02-26 17:24:56 -0300

    Tratando erros com custom exceptions

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,16 +1,27 @@
-﻿namespace xadrez_console;
+﻿using System.Security.Cryptography;
+
+namespace xadrez_console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-        tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-        tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-        tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+        // bool x = true;
+        // Console.WriteLine(!x);
+        try
+        {
+            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(8, 8));
 
+            Tela.ImprimirTabuleiro(tabuleiro);
+        }
+        catch (TabuleiroException x)
+        {
+            Console.WriteLine(x.Message);
+        }
 
-        Tela.ImprimirTabuleiro(tabuleiro);
         int[,] matriz = new int[3, 4];
     }
 }
---
+++ xadrez_console/Tabuleiro/Exceptions/TabuleiroException.cs
@@ -0,0 +1,6 @@
+﻿namespace xadrez_console;
+
+public class TabuleiroException : ApplicationException
+{
+    public TabuleiroException(string msg) : base(msg) { }
+}
